8354fe09ad674713ed661bca05e11c4dcab54801
Nizam <ramazanov570633@gmail.com>
2024-01-22 22:59:27 +0400

    first commit

---
+++ lib/Search/search.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SearchWidget extends StatefulWidget {
+  const SearchWidget({super.key});
+
+  @override
+  State<SearchWidget> createState() => _SearchWidgetState();
+}
+
+class _SearchWidgetState extends State<SearchWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 7, 1, 41),
+      ),
+      home: const Scaffold(
+        body: Text(
+          'Salam',
+          style: TextStyle(color: Colors.amber),
+        ),
+      ),
+    );
+  }
+}
